3a505bfd9854610760b514f3da0d8f815b728124
WineHua <winehua@localhost>
2026-07-18 18:46:46 +0800

    winehua: avoid duplicate MinGW d3d10 UUID

--- src/d3d10/d3d10_interfaces.h
+++ src/d3d10/d3d10_interfaces.h
@@ -4,6 +4,8 @@
 
 #ifdef _MSC_VER
 struct __declspec(uuid("0803425a-57f5-4dd6-9465-a87570834a08")) ID3D10StateBlock;
-#else
+#elif !defined(__MINGW32__)
+/* Older non-MinGW headers do not declare the UUID.  Modern MinGW's
+ * d3d10effect.h already supplies it, so redeclaring it causes a hard error. */
 __CRT_UUID_DECL(ID3D10StateBlock, 0x0803425a,0x57f5,0x4dd6,0x94,0x65,0xa8,0x75,0x70,0x83,0x4a,0x08);
 #endif
