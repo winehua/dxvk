bea4b847001c44c86e78be499bea3d83b29f4c5a
WineHua <winehua@localhost>
2026-07-23 03:45:57 +0800

    dxvk: avoid mutable images for concrete BC formats

--- src/dxgi/dxgi_format.cpp
+++ src/dxgi/dxgi_format.cpp
@@ -901,10 +901,22 @@ namespace dxvk {
 
       const DXGI_VK_FORMAT_FAMILY rgba8Family = {
         VK_FORMAT_R8G8B8A8_UNORM, VK_FORMAT_R8G8B8A8_SRGB };
+      const DXGI_VK_FORMAT_FAMILY rgba8UnormFamily = {
+        VK_FORMAT_R8G8B8A8_UNORM };
+      const DXGI_VK_FORMAT_FAMILY rgba8SrgbFamily = {
+        VK_FORMAT_R8G8B8A8_SRGB };
       const DXGI_VK_FORMAT_FAMILY r8Family = {
         VK_FORMAT_R8_UNORM, VK_FORMAT_R8_SNORM };
       const DXGI_VK_FORMAT_FAMILY rg8Family = {
         VK_FORMAT_R8G8_UNORM, VK_FORMAT_R8G8_SNORM };
+      const DXGI_VK_FORMAT_FAMILY r8UnormFamily = {
+        VK_FORMAT_R8_UNORM };
+      const DXGI_VK_FORMAT_FAMILY r8SnormFamily = {
+        VK_FORMAT_R8_SNORM };
+      const DXGI_VK_FORMAT_FAMILY rg8UnormFamily = {
+        VK_FORMAT_R8G8_UNORM };
+      const DXGI_VK_FORMAT_FAMILY rg8SnormFamily = {
+        VK_FORMAT_R8G8_SNORM };
       const DXGI_VK_FORMAT_FAMILY rgba16fFamily = {
         VK_FORMAT_R16G16B16A16_SFLOAT };
 
@@ -916,8 +928,27 @@ namespace dxvk {
         m_dxgiFamilies[uint32_t(format)] = rgba8Family;
 
       m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC4_TYPELESS)] = r8Family;
+      m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC4_UNORM)] = r8UnormFamily;
+      m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC4_SNORM)] = r8SnormFamily;
       m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC5_TYPELESS)] = rg8Family;
+      m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC5_UNORM)] = rg8UnormFamily;
+      m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC5_SNORM)] = rg8SnormFamily;
       m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC6H_TYPELESS)] = rgba16fFamily;
+      m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC6H_UF16)] = rgba16fFamily;
+      m_dxgiFamilies[uint32_t(DXGI_FORMAT_BC6H_SF16)] = rgba16fFamily;
+
+      // Concrete formats do not need mutable images. Keeping a one-format
+      // family avoids an unnecessary Venus mutable-image capability query,
+      // while typeless resources retain the unorm/snorm or sRGB family above.
+      for (DXGI_FORMAT format : {
+             DXGI_FORMAT_BC1_UNORM, DXGI_FORMAT_BC2_UNORM,
+             DXGI_FORMAT_BC3_UNORM, DXGI_FORMAT_BC7_UNORM })
+        m_dxgiFamilies[uint32_t(format)] = rgba8UnormFamily;
+
+      for (DXGI_FORMAT format : {
+             DXGI_FORMAT_BC1_UNORM_SRGB, DXGI_FORMAT_BC2_UNORM_SRGB,
+             DXGI_FORMAT_BC3_UNORM_SRGB, DXGI_FORMAT_BC7_UNORM_SRGB })
+        m_dxgiFamilies[uint32_t(format)] = rgba8SrgbFamily;
 
       Logger::info("WineHua: BC1-BC7 upload-time decompression enabled");
     }
